00b78c491f6f35a5c68e759eacecdc3e3bd822da
Chipper Snyder <chippersnyder@chip-MacBook-Pro.local>
2025-12-04 11:26:15 -0500

    Remove unused import

--- components/ImageCropper.tsx
+++ components/ImageCropper.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState, useEffect, useRef, ErrorInfo } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import Cropper from 'react-easy-crop'
 import { X, Check } from 'lucide-react'
 import 'react-easy-crop/react-easy-crop.css'
